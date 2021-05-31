974958b3fb19d72c116b8f32b5dd3935a25a2d78
Rehtt <dsreshiram@gmail.com>
2021-05-31 10:49:59 +0800

    ffmpeg

---
+++ ffmpeg.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "使用root运行"
+    exit
+fi
+apt update
+apt upgrade -y
+
+if [[ `lspci | grep NVIDIA` ]]; then
+    curl -s https://raw.githubusercontent.com/Rehtt/AWS_Script/main/nvidia.sh | bash -
+fi
+
+apt install ffmpeg
+curl -L https://github.com/Rehtt/AWS_Script/raw/main/font/WeiRuanYaHei-1.ttf -o /usr/share/fonts/truetype/dejavu/WeiRuanYaHei-1.ttf
+fc-cache -fv
