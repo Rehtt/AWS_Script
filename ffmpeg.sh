3f8a8236ccf34ed6e64416c3357732425ac2e3b0
Rehtt <dsreshiram@gmail.com>
2021-05-31 11:34:26 +0800

    update

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -11,6 +11,6 @@ if [[ `lspci | grep NVIDIA` ]]; then
     curl -s https://raw.githubusercontent.com/Rehtt/AWS_Script/main/nvidia.sh | bash -
 fi
 
-apt install ffmpeg
+apt install ffmpeg -y
 curl -L https://github.com/Rehtt/AWS_Script/raw/main/font/WeiRuanYaHei-1.ttf -o /usr/share/fonts/truetype/dejavu/WeiRuanYaHei-1.ttf
 fc-cache -fv
